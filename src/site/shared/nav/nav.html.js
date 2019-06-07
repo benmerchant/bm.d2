cc7a34258924e87cdbb5fff8954580e59bcf3680
Ben Merchant <benjaminmerchant@gmail.com>
2019-06-07 15:08:30 -0400

    closes #17

--- src/site/shared/nav/nav.html.js
+++ src/site/shared/nav/nav.html.js
@@ -30,7 +30,7 @@ export const navTemplate = `
     <!-- Filter the Cards -->
       <input ng-model="cardSearch" class="form-control mr-sm-2" type="search" placeholder="Search">
 
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
 </nav>
