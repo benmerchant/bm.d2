7122dc06bff709cd960ee3702917980ab966f750
Ben Merchant <benjaminmerchant@gmail.com>
2019-06-07 15:06:32 -0400

    closes #24 closes #21 closes #19 add conditional formatting-> linklister

--- src/site/features/linkLister/linksInFakeMongo.js
+++ src/site/features/linkLister/linksInFakeMongo.js
@@ -1,6 +1,6 @@
 export const linksInFakeMongo = [
-  {"url": "google.com"},
-  {"url": "apple.com"},
-  {"url": "twit.tv/"},
-  {"url": "benmerchant.github.io"}
+  {"url": "google.com", "daysAgo": 25},
+  {"url": "apple.com", "daysAgo": 3},
+  {"url": "twit.tv", "daysAgo": 14},
+  {"url": "benmerchant.github.io", "daysAgo": 1}
 ];
