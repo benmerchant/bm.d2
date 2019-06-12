fbd5e5c97302f28e27ceeafef8479ac2dd6abbf3
Ben Merchant <benjaminmerchant@gmail.com>
2019-06-12 03:51:28 -0400

    maybe later Todd Motto. Going back to main sprint branch

--- source/start.js
+++ source/start.js
@@ -1,9 +1,96 @@
 import * as angular from 'angular';
 import uiRouter from '@uirouter/angularjs';
+import newStories from './storiesInFakeMongo';
 
-angular.module('application', [uiRouter])
-  .component('ground', {
-    template: `<div class="">Hey</div>`
-  }).config([() => {
+class StoryService {
+  constructor(allStoriesFromDB){
+    this.getAllStoriesList = allStoriesFromDB;
+  }
+  getAllStoriesList(){
+    return this.getAllStoriesList;
+  }
+}
 
-  }]);
+const ground = angular.module('application', [uiRouter]);
+
+const groundState = {
+  name: 'ground',
+  redirectTo: 'app',
+  component: 'groundComponent'
+}
+const appState = {
+  parent: 'ground',
+  name: 'app',
+  url: '/app',
+  component: 'mainAppComponent'
+}
+
+ground.config(['$uiRouterProvider', ($uiRouter) => {
+  $uiRouter.trace.enable(1);
+  const $urlService = $uiRouter.urlService;
+  $urlService.rules.otherwise({ state: 'app' });
+
+  const $stateRegistry = $uiRouter.stateRegistry;
+  $stateRegistry.register(groundState);
+  $stateRegistry.register(appState);
+
+}]);
+
+ground.component('groundComponent',{
+    template: `<div class="one">
+      <div ui-view>Hey inside ui-view</div>
+    </div>`
+  });
+ground.component('mainAppComponent', {
+      template: `
+      <div class="">
+        <nav-component></nav-component>
+          <div class="main">
+            <div ui-view>Hey inside ui-view 2</div
+          </div>
+      </div>`
+    });
+
+
+
+// const app = angular.module('mainApp',[uiRouter])
+// .config(($stateProvider) => {
+  //   $stateProvider('mainAppState', {
+  //     redirectTo: 'allStoriesState',
+  //     url: '/app',
+  //     component: mainAppComponent
+  //   });
+  // });
+//
+// const allStories = angular.module('allStories',[uiRouter])
+//   .component('allStories', {
+//     bindings: {
+//       allStories: '<'
+//     },
+//     template: `
+//     <div class="stories"> <h1>Stories </h1>
+//     <ul class="stories-list">
+//       <li ng-repeat="story in $ctrl.stories">
+//         {{story.title}}
+//       </li>
+//     </ul>
+//     </div>`,
+//     controller: class AllStoriesComponent {
+//       constructor($state){
+//         this.$state = $state;
+//         console.log($state);
+//       }
+//     }
+//   }).config(($stateProvider) => {
+//     $stateProvider.state('allStoriesState', {
+//       parent: 'mainAppState',
+//       url: '/allStories',
+//       component: 'allStories',
+//       resolve: {
+//         stories(StoryService){
+//           console.log(StoryService.getAllStoriesList());
+//           return StoryService.getAllStoriesList();
+//         }
+//       }
+//     });
+//   }).service('StoryService', StoryService);
---
+++ source/storiesInFakeMongo.js
@@ -0,0 +1,38 @@
+const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+function getRandomInt(min, max) {
+  min = Math.ceil(min); max = Math.floor(max);
+  //The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min)) + min;
+}; // fron MSDN
+const fakeStoriesFromMongo = [
+  { title: 'Community', category: 'about',
+    body_copy: 'Graduated College. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+  { title: 'Restaurant Machine', category: 'projects',
+    body_copy: 'Start POS. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+  { title: 'CheckiO', category: 'tutorials',
+    body_copy: 'First Challenge. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+  { title: 'The future', category: 'articles',
+    body_copy: 'Working on Portfolio. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+  { title: 'My thoughts', category: 'about',
+    body_copy: 'Love my mom. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+  { title: 'express API', category: 'projects',
+    body_copy: 'How to do it?. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+  { title: 'NodeJS', category: 'tutorials',
+    body_copy: 'compiled language?. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+  { title: 'What have I learned', category: 'blog',
+    body_copy: 'Love the internet. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+  { title: 'first computer built', category: 'about',
+    body_copy: 'build a PIC. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+  { title: 'Hangman', category: 'projects',
+    body_copy: 'javascript class games. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+];
+// make some programatticly created dates
+const storyMap = fakeStoriesFromMongo.map(story => {
+  const year = getRandomInt(2015, 2020);
+  const month = getRandomInt(0,12);
+  const day = getRandomInt(0, 28)+1; // dont need to waste time with if statements
+  const randomDate = new Date(months[month] + day + ',' + year);
+  story['date'] = randomDate;
+});
+
+export const newStories = fakeStoriesFromMongo;
